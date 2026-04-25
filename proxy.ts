6cb2b48c256352bce9ce05682c93b4dce20be692
Maithalee Vishe <maithaleevishe@Maithalees-MacBook-Air.local>
2026-04-25 21:35:21 +0530

    fix: use correct NextAuth

--- proxy.ts
+++ proxy.ts
@@ -2,12 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function proxy(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const isProduction = process.env.NODE_ENV === "production";
+
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    cookieName: isProduction
+      ? "__Secure-authjs.session-token"
+      : "authjs.session-token",
+    secureCookie: isProduction,
+  });
+
   if (!token) {
     const signIn = new URL("/auth/signin", req.url);
     signIn.searchParams.set("callbackUrl", req.nextUrl.pathname);
     return NextResponse.redirect(signIn);
   }
+
   return NextResponse.next();
 }
 
